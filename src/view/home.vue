44e9ef66199113b63ad9d9ceb405061d7902a3cb
renxj <budingfei@126.com>
2020-09-24 19:12:48 +0800

    联调完成

--- src/view/home.vue
+++ src/view/home.vue
@@ -5,21 +5,21 @@
       <div class="center">
         <div class="item">
           <div class="cont">
-            <p class="text">3</p>
+            <p class="text">{{miners.length}}</p>
             <p class="label">矿机数</p>
           </div>
           <img src="../assets/images/img_miner.png" />
         </div>
         <div class="item">
           <div class="cont">
-            <p class="text">18.7M</p>
+            <p class="text">{{totalPower}}</p>
             <p class="label">算力</p>
           </div>
           <img src="../assets/images/img_power.png" />
         </div>
         <div class="item">
           <div class="cont">
-            <p class="text">3</p>
+            <p class="text">{{amount_cur/1000}}</p>
             <p class="label">未分账</p>
           </div>
           <img src="../assets/images/img_coin.png" />
@@ -28,15 +28,15 @@
           <div class="cont">
             <div class="num">
               <span>高度：</span>
-              <b>2132</b>
+              <b>{{height}}</b>
             </div>
             <div class="num">
               <span>矿池算力：</span>
-              <b>2132</b>
+              <b>{{power}}</b>
             </div>
             <div class="num">
               <span>总矿工数：</span>
-              <b>2132</b>
+              <b>{{miner}}</b>
             </div>
           </div>
         </div>
@@ -44,28 +44,34 @@
 
       <div class="block">
         <h3 class="title">账单列表</h3>
-        <el-table :data="tableData" style="width: 100%">
-          <el-table-column prop="date" label="时间" width="180">
+        <el-table :data="transfers" style="width: 100%">
+          <el-table-column prop="depend" label="时间" width="180">
           </el-table-column>
-          <el-table-column prop="name" label="数量(SAT)" width="180">
+          <el-table-column label="数量(SAT)" width="180">
+            <template slot-scope="scope">
+              {{scope.row.amount/10000}}
+            </template>
           </el-table-column>
-          <el-table-column prop="address" label="交易 ID">
+          <el-table-column prop="hash" label="交易 ID">
           </el-table-column>
-          <el-table-column prop="address" label="状态" width="180">
+          <el-table-column label="状态" width="180">
+            <template slot-scope="scope">
+              {{scope.row.hash ? '完成' : '正在转'}}
+            </template>
           </el-table-column>
         </el-table>
       </div>
 
       <div class="block">
         <h3 class="title">矿机列表</h3>
-        <el-table :data="tableData" style="width: 100%">
-          <el-table-column prop="date" label="编号">
+        <el-table :data="miners" style="width: 100%">
+          <el-table-column type="index" label="编号" width="80">
           </el-table-column>
-          <el-table-column prop="name" label="矿机名">
+          <el-table-column prop="number" label="矿机名">
           </el-table-column>
-          <el-table-column prop="address" label="实时算力">
+          <el-table-column prop="power_cur" label="实时算力">
           </el-table-column>
-          <el-table-column prop="address" label="平均算力">
+          <el-table-column prop="power_average" label="平均算力">
           </el-table-column>
         </el-table>
       </div>
@@ -81,15 +87,46 @@ export default {
   name: "home",
   data() {
     return {
-      tableData: [],
+      miners: [], // 矿机列表
+      transfers: [], // 账单列表
+      totalPower: "", // 算力
+      amount_cur: "", // 未分账
+      power: "", // 矿池算力：
+      miner: "", // 总矿工数
+      height: "", // 高度
     };
   },
   components: {
     Footer,
     Header,
   },
-  methods: {},
-  mounted() {},
+  methods: {
+    queryData() {
+      let url = "http://www.iobft.com:8101/Stats?style=2";
+      this.axios.get(url).then((res) => {
+        console.log(res.data);
+        let arr = res.data.split(" ");
+        console.log(arr);
+        this.height = arr[0].split(":")[1];
+        this.power = arr[1].split(":")[1];
+        this.miner = arr[2].split(":")[1];
+      });
+    },
+    queryTableData() {
+      let url = "http://www.iobft.com:8101/Command?input=miner%20Y7ECVSUDQyiaoditKMbEEyu1KaoQiLGMj";
+      this.axios.get(url).then((res) => {
+        console.log(res.data);
+        this.miners = res.data.miners;
+        this.transfers = res.data.transfers;
+        this.totalPower = res.data.totalPower;
+        this.amount_cur = res.data.amount_cur;
+      });
+    },
+  },
+  mounted() {
+    this.queryData();
+    this.queryTableData();
+  },
   destroyed() {},
 };
 </script>
@@ -192,7 +229,7 @@ export default {
         width: 280px;
       }
       &:nth-child(4) {
-        width: 180px;
+        width: 200px;
       }
     }
   }
